6eef748c378af5df0d6b642e812fd0388fab6cf8
ABCaps35 <acapelli345@gmail.com>
2019-09-11 16:32:51 -0400

    Updates node and topic names

--- src/publisher_code.cpp
+++ src/publisher_code.cpp
@@ -4,9 +4,9 @@
 
 int main(int argc, char** argv)
 {
-	ros::init(argc, argv, "talker");
+	ros::init(argc, argv, "publisher_node");
 	ros::NodeHandle n;
-	ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter",1000);
+	ros::Publisher chatter_pub = n.advertise<std_msgs::String>("publisher_topic",1000);
 	ros::Rate loop_rate(1);
 	int count=0;
 	while(ros::ok())
